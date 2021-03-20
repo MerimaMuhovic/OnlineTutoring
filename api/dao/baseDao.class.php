0155784a8cabdcc0591a8eadfb732e91ba74e615
MerimaMuhovic <merima.muhovic@hotmail.com>
2021-03-20 14:10:31 +0100

    commit

---
+++ api/dao/baseDao.class.php
@@ -0,0 +1,23 @@
+<?php
+class baseDao{
+    public function __construct(){
+
+
+    }
+    public function insert(){
+
+        
+    }
+    public function update(){
+        
+    }
+
+    public function query(){
+
+    }
+
+   public function query_unique{
+
+   }
+}
+?>
