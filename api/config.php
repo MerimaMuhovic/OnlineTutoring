8da5b8686d5add20a7a8734f85c4ecc42a57cbab
MerimaMuhovic <merima.muhovic@hotmail.com>
2021-03-20 20:52:44 +0100

    commit

--- api/config.php
+++ api/config.php
@@ -3,9 +3,9 @@
 class Config {
 
     const DB_HOST ="localhost";
-    const DB_USERNAME ="root";
-    const DB_PASSWORD="root123";
-    const DB_SCHEME="onlineturoting";
+    const DB_USERNAME ="onlinetutoring";
+    const DB_PASSWORD="onlinetutoring";
+    const DB_SCHEME="onlinetutoring";
 
 }
 
