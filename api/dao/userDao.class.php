6ce2fe6ea4ffa3433fd275626b390227738816fa
MerimaMuhovic <merima.muhovic@hotmail.com>
2021-03-21 00:22:30 +0100

    commit

--- api/dao/userDao.class.php
+++ api/dao/userDao.class.php
@@ -15,6 +15,17 @@ class UserDao extends BaseDao{
 
     }
 
+    public function addUser($user){
+
+        $sql = "INSERT INTO users (name, surname, email, password,account_id) VALUES (:name, :surname, :email, :password, :account_id)";
+        $stmt = $this->connection-> prepare($sql);
+        $stmt->execute($user);
+
+    }
+    public function updateUser($id, $user){
+
+    }
+
 }
 
 ?>
--- api/test.php
+++ api/test.php
@@ -3,9 +3,18 @@ require_once dirname(__FILE__)."/dao/UserDao.class.php";
 
 $userDao= new UserDao();
 
-$user= $userDao->getUserByID(2);
+//$user= $userDao->getUserByID(2);
 
 //$userDao->getUserByEmail("merima.muhovic@hotmail.com");
+
+$user=[
+"name"=>"Alma",
+"surname"=>"Spahic",
+"email"=>"alma@hotmail.com",
+"password"=>"0000",
+"account_id"=> 3
+];
+$userDao->addUser($user);
 
 print_r($user);
 
