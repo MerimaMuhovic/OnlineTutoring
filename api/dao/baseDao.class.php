60edbdb52b6a05c1d720f93bb9fcbbc93f6dcce3
MerimaMuhovic <merima.muhovic@hotmail.com>
2021-03-21 11:32:07 +0100

    commit

--- api/dao/baseDao.class.php
+++ api/dao/baseDao.class.php
@@ -2,10 +2,10 @@
 require_once dirname(__FILE__)."/../config.php";
 
 class BaseDao{
-
-    protected $connection;
+  
+  protected $connection;
+  
     public function __construct(){
-   
 
 try {
   $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME,Config::DB_USERNAME, Config::DB_PASSWORD);
--- api/test.php
+++ api/test.php
@@ -3,7 +3,7 @@ require_once dirname(__FILE__)."/dao/UserDao.class.php";
 
 $userDao= new UserDao();
 
-//$user= $userDao->getUserByID(2);
+//$user= $userDao->getUserByID(1);
 
 //$userDao->getUserByEmail("merima.muhovic@hotmail.com");
 
@@ -12,8 +12,8 @@ $user=[
 "name" => "Alma",
 "surname" => "Spahic",
 "email" => "alma@hotmail.com",
-"password" => "0000",
-"account_id" => 3
+"password" => "1234",
+"account_id" => 5
 
 ];
 $userDao->addUser($user);
